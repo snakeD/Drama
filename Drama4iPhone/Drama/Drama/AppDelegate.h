678389693960cb6cd18caa8b225cefbb6c1ddd24
DSnake <zt1215.d@gmail.com>
2013-12-01 16:13:29 +0800

    新建项目

---
+++ Drama4iPhone/Drama/Drama/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Drama
+//
+//  Created by SnakeD on 13-12-1.
+//  Copyright (c) 2013年 DSnake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Drama4iPhone/Drama/DramaTests/DramaTests.m
@@ -0,0 +1,34 @@
+//
+//  DramaTests.m
+//  DramaTests
+//
+//  Created by SnakeD on 13-12-1.
+//  Copyright (c) 2013年 DSnake. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DramaTests : XCTestCase
+
+@end
+
+@implementation DramaTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
